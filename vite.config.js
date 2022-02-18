ea03a0bbca8be913028dae02a3c4ad85d939464e
Grant Walker <GrantW2505@gmail.com>
2022-02-18 23:09:16 +0200

    updated vite config to include additional examples

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        1: resolve(__dirname, 'newbie', '3-column-preview-card-component/index.html')
+        1: resolve(__dirname, 'newbie', '3-column-preview-card-component/index.html'),
+        2: resolve(__dirname, 'newbie', 'order-summary-component/index.html'),
+        3: resolve(__dirname, 'newbie', 'social-proof-section/index.html')
       }
     }
   }
